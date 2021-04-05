b750b11bca0f9843ea9047957e9301fb767d295e
Danielov1 <63906099+danielov1@users.noreply.github.com>
2021-04-05 13:56:30 +0300

    Update main.py

--- Opsgenie/main.py
+++ Opsgenie/main.py
@@ -1,32 +1,32 @@
 #! /usr/bin/env python
 
-import xlrd ## pip install xlrd==1.2.0, This version supported xlsx files
-from xlrd import open_workbook
-from datetime import time
+import pandas as pd
+import json
 import requests
 import sys
-import json
 from datetime import datetime
 from pytz import timezone
 import datetime
 import pytz
 
+loc = '~/Desktop/opsgenie/shifts.xlsx'
+schedule_id = 'c3f9e3b6-d717-424b-8085-5dfad8b4560e'
+genie_token = 'f0c1430d-ee72-4dcc-ab49-e76b01d3ec75'
 
-class Arm(object):
-    def __init__(self, StartDate, EndDate, startTime, endTime, RotationName, RotationID, Name, User):
-        self.StartDate = StartDate
-        self.EndDate = EndDate
-        self.startTime = startTime
-        self.endTime = endTime
-        self.RotationName = RotationName
-        self.RotationID = RotationID
-        self.Name = Name
-        self.User = User
+def prefix_time(x):
+    x = list(x)
+    prefix_zero = x[0]
+    prefix_zero = str(prefix_zero)
+    if prefix_zero == '0':
+        prefix_zero = x[1]
+        return prefix_zero
+    else:
+        x = ''.join(x)
+        return x
 
-    def Arm(self):
-        return[self.StartDate, self.EndDate, self.startTime, self.endTime, self.RotationName, self.RotationID, self.Name, self.User]
 
-## function that change TZ
+## Function that convert time from Asia/Jerusalem to UTC
+
 def change_time(m_date, m_time):
     time_zone = timezone('Asia/Jerusalem')
     my_time = m_date + " " + m_time
@@ -39,91 +39,95 @@ def change_time(m_date, m_time):
     date_split = split_date[0]
     return time_split, date_split
 
-## Open Excel file
-loc = 'excelpath'
-wb = open_workbook(loc)
+## Function that sort Dataframe by Rotation Name and execute PATCH API call that update the schedule
 
-## Read from Excel file and call to Class named Arm
-for sheet in wb.sheets():
-    number_of_rows = sheet.nrows
-    number_of_columns = sheet.ncols
+def sort_df(rotation_name, rotation_id):
 
-    items = []
+    user_list = []
+    start_date_list = []
+    start_time_list = []
+    end_date_list = []
+    end_time_list = []
 
-    rows = []
-    for row in range(1, number_of_rows):
-        values = []
-        for col in range(number_of_columns):
-            value  = (sheet.cell(row,col).value)
-            values.append(value)
-        item = Arm(*values)
-        items.append(item)
+    df = pd.read_excel(loc)
 
-for item in items:
-    start_datels = list(item.Arm())[0]
-    end_datels = list(item.Arm())[1]
-    start_timels = list(item.Arm())[2]
-    end_timels = list(item.Arm())[3]
-    rotation_namels = list(item.Arm())[4]
-    rotation_IDls = list(item.Arm())[5]
-    namels = list(item.Arm())[6]
-    userls = list(item.Arm())[7]
-    print(start_timels, start_datels)
+    rslt_df = df[df["RotationName"] == rotation_name] 
+    df = rslt_df.groupby(['StartDate', 'StartTime', 'EndDate', 'EndTime', 'RotationID', 'Users']).agg(tuple).reset_index()
+    # print(df)
+    for record in df.to_dict(orient='records'):
+        
+        start_date_list.append(str(record['StartDate']).split()[0])
+        start_time_list.append(str(record['StartTime']))
+        end_date_list.append(str(record['EndDate']).split()[0])
+        end_time_list.append(str(record['EndTime']))
+        user_list.append(str(record['Users']))
 
 
-    ## convert startDate format
-    xl_date_start = start_datels
-    datetime_date = xlrd.xldate_as_datetime(xl_date_start, 0)
-    date_object = datetime_date.date()
-    my_startDate = date_object.isoformat()
-    my_startDate = str(my_startDate)
+    ## Get the first Start Date and the first Start Time
+
+    start_date = start_date_list[0]
+    start_time = start_time_list[0]
+    prefix_start_time_h = start_time.split(":")[0]
+    prefix_start_time_m = start_time.split(":")[1] 
+    prefix_start_time_h = prefix_time(prefix_start_time_h)
+    prefix_start_time_m = prefix_time(prefix_start_time_m)
+
+    ## Get the last End Date and the last End Start Time
+
+    end_date = end_date_list[-1]
+    end_time = end_time_list[-1]
+    prefix_end_time_h = end_time.split(":")[0]
+    prefix_end_time_m = end_time.split(":")[1] 
+    prefix_end_time_h = prefix_time(prefix_end_time_h)
+    prefix_end_time_m = prefix_time(prefix_end_time_m)
+
+    ## Convert time and date to UTC
+
+    time_split, date_split = change_time(start_date, start_time)
+    my_start_date_split = date_split
+    my_start_time_split = time_split
+
+    time_split, date_split = change_time(end_date, end_time)
+    my_end_date_split = date_split
+    my_end_time_split = time_split
+    
+
+    ## Get User List to JSON
+    
+    user_list_json = {"participants": [{ "type": "user", "username": i} for i in user_list]}
+    json.dumps(user_list_json)
+    user_list_json = str(user_list_json)
+    user_list_json = user_list_json[1:-1]
+    user_list_json = user_list_json.replace("'",'"')
 
 
-    ## convert endDate format
-    xl_date_end = end_datels
-    datetime_date = xlrd.xldate_as_datetime(xl_date_end, 0)
-    date_object = datetime_date.date()
-    my_endDate = date_object.isoformat()
-    my_endDate = str(my_endDate)
+    ## PATCH API call that update the schedule
+    
+    print(my_start_date_split,my_start_time_split)
+    print(my_end_date_split,my_end_time_split)
 
-
-    ## convert startTime format
-    s = start_timels 
-    s = int(s * 24 * 3600) 
-    my_startTime = time(s//3600, (s%3600)//60, s%60) # hours, minutes, seconds
-    my_startTime = str(my_startTime)
-
-
-    ## convert endTime format
-    e = end_timels 
-    e = int(e * 24 * 3600) 
-    my_endTime = time(e//3600, (e%3600)//60, e%60) # hours, minutes, seconds
-    my_endTime = str(my_endTime)
-
-    ## convert to UTC TZ
-    time_split, date_split = change_time(my_startDate, my_startTime)
-    my_startDate = date_split
-    my_startTime = time_split
-
-    time_split, date_split = change_time(my_endDate, my_endTime)
-    my_endDate = date_split
-    my_endTime = time_split
-
-    print(my_startDate, my_startTime)
-    print(my_endDate, my_endTime)
-
-
-    # PATCH call request to Opsgenie
     headers = {
-        'Authorization': 'GenieKey <key>,
+        'Authorization': 'GenieKey '+ genie_token,
         'Content-Type': 'application/json',
-        }
+    }
 
-    data = ' { "name": "'+ rotation_namels +'", "startDate": "'+ my_startDate +'T'+ my_startTime + 'Z", "endDate": "'+ my_endDate +'T'+ my_endTime + 'Z", "type": "daily", "length": 1, "participants": [ { "type": "user", "username": "'+ userls +'" } ] }'
+    data = ' { "name": "'+ rotation_name +'", "startDate": "'+ my_start_date_split +'T'+ my_start_time_split + 'Z", "endDate": "'+ my_end_date_split +'T'+ my_end_time_split + 'Z", "type": "daily", "length": 1, '+ user_list_json + ', "timeRestriction": { "type": "time-of-day", "restriction": { "startHour": '+ prefix_start_time_h +', "endHour": '+ prefix_end_time_h +', "startMin": '+ prefix_start_time_m +', "endMin": '+ prefix_end_time_m +' } } }'
+    response = requests.patch('https://api.eu.opsgenie.com/v2/schedules/'+ schedule_id +'/rotations/'+ rotation_id, headers=headers, data=data)
 
-    response = requests.patch('https://api.eu.opsgenie.com/v2/schedules/<schedule>/rotations/'+ rotation_IDls +'', headers=headers, data=data)   
     response_Org = response.text
     sys.stdout.flush()
     response_json = json.loads(response_Org)
     print(response_json)
-    print(my_startTime, my_endTime)
+    print(data)
+    
+
+## Read excel file to Dataframe
+df = pd.read_excel(loc)
+df = df.groupby(['RotationName', 'RotationID']).agg(tuple).reset_index()
+
+## Run function for each Rotation name
+
+for record in df.to_dict(orient='records'):
+    rotation_name = (str(record['RotationName']))
+    rotation_id = (str(record['RotationID']))
+    sort_df(rotation_name, rotation_id)
